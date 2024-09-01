068fea55ae8d62676bb7a20be2e7a6849f079fc7
Peter <guitaronline89@gmail.com>
2024-09-01 21:52:56 +0300

    migrating database from Postgres to MySql

--- src/main/java/com/builder/securedoc/service/impl/EmailServiceImpl.java
+++ src/main/java/com/builder/securedoc/service/impl/EmailServiceImpl.java
@@ -2,7 +2,6 @@ package com.builder.securedoc.service.impl;
 
 import com.builder.securedoc.exception.ApiException;
 import com.builder.securedoc.service.EmailService;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
@@ -14,33 +13,36 @@ import static com.builder.securedoc.utils.EmailUtils.getEMailMessage;
 import static com.builder.securedoc.utils.EmailUtils.getResetPasswordMessage;
 
 @Service
-@RequiredArgsConstructor
 @Slf4j
 public class EmailServiceImpl implements EmailService {
 
     private static final String NEW_USER_ACCOUNT_VERIFICATION = "New User Account Verification";
     private static final String PASSWORD_RESET_REQUEST = "Reset Password Request";
-    @Value(value = "${spring.mail.username}")
+
     private final JavaMailSender sender;
-    @Value(value = "${spring.mail.verify.host}")
-    private String host;
-    private String formEmail;
+    private final String formEmail;
+    private final String host;
+
+    public EmailServiceImpl(JavaMailSender sender,
+                            @Value("${spring.mail.username}") String formEmail,
+                            @Value("${spring.mail.verify.host}") String host) {
+        this.sender = sender;
+        this.formEmail = formEmail;
+        this.host = host;
+    }
+
     @Override
     @Async
     public void sendNewAccountEmail(String name, String email, String token) {
-
         try {
-
             var message = new SimpleMailMessage();
             message.setSubject(NEW_USER_ACCOUNT_VERIFICATION);
             message.setFrom(formEmail);
             message.setTo(email);
             message.setText(getEMailMessage(name, host, token));
-
             sender.send(message);
         } catch (Exception e) {
-
-            log.error(e.getMessage());
+            log.error("Error sending new account email: {}", e.getMessage());
             throw new ApiException("Unable to send email.");
         }
     }
@@ -48,19 +50,15 @@ public class EmailServiceImpl implements EmailService {
     @Override
     @Async
     public void sendPasswordResetEmail(String name, String email, String token) {
-
         try {
-
             var message = new SimpleMailMessage();
             message.setSubject(PASSWORD_RESET_REQUEST);
             message.setFrom(formEmail);
             message.setTo(email);
             message.setText(getResetPasswordMessage(name, host, token));
-
             sender.send(message);
         } catch (Exception e) {
-
-            log.error(e.getMessage());
+            log.error("Error sending password reset email: {}", e.getMessage());
             throw new ApiException("Unable to send email.");
         }
     }
